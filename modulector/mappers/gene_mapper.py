d3925867ef81616062b4b239151dce84b7d35178
agusmarraco <adm101195@gmail.com>
2020-09-22 23:47:02 -0300

    adding more translations to txt and updating test jsons

--- modulector/mappers/gene_mapper.py
+++ modulector/mappers/gene_mapper.py
@@ -14,16 +14,16 @@ class GeneMapper:
     def execute(self, ref_seq_array, old_ref_seq_array):
         translations = load_translations()
         missing_translations = []
-        for ref in ref_seq_array:
-            if ref not in translations.keys() and ref not in old_ref_seq_array:
-                missing_translations.append(ref)
-        print("missing ref" + str(len(missing_translations)))
-        data_to_append = ""
-        translated_from_api = translate_ref_seq(missing_translations)
-        for key in translated_from_api.keys():
-            data_to_append += translated_from_api[key] + "\t" + key + "\n"
-        with open(path, "a") as f:
-            f.write(data_to_append)
+        # for ref in ref_seq_array:
+        #     if ref not in translations.keys() and ref not in old_ref_seq_array:
+        #         missing_translations.append(ref)
+        # print("missing ref" + str(len(missing_translations)))
+        # data_to_append = ""
+        # translated_from_api = translate_ref_seq(missing_translations)
+        # for key in translated_from_api.keys():
+        #     data_to_append += translated_from_api[key] + "\t" + key + "\n"
+        # with open(path, "a") as f:
+        #     f.write(data_to_append)
         with transaction.atomic():
             insert_query_prefix = f'INSERT INTO modulector_genesymbolmapping (refseq, symbol) VALUES '
             insert_template = "('{}','{}')"
