a1f741c06760dcb4fed939b075b09480cf01cc66
agustinmarraco <marraco.agustin@gmail>
2021-02-11 01:20:55 -0300

    using mirdip as file name for processor

--- modulector/processors/mirdip_processor.py
+++ modulector/processors/mirdip_processor.py
@@ -26,7 +26,7 @@ def process(mirna_source: MirnaSource):
     mirna gene table
     """
     # Do to the size of this file, you should download it from the release files section. It is not in the repo.
-    file_path = os.path.join(parent_dir, "files/test.txt")
+    file_path = os.path.join(parent_dir, "files/mirdip.txt")
     logger.info("loading data for i2d")
     # loading files
     series_names = mirna_source.mirnacolumns.order_by('position').values_list('field_to_map', flat=True)
