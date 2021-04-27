a0e03ba53f050a140527e87c71de40560e275c42
Genarito <genarocamele@hotmail.com>
2021-04-27 09:02:11 -0300

    + Replaced Timezone.now() by auto_now_add=True

--- modulector/models.py
+++ modulector/models.py
@@ -1,8 +1,6 @@
 from typing import Optional
-
 from django.db import models
 from django.db.models import QuerySet
-from django.utils.timezone import now
 
 
 class DatasetSeparator(models.TextChoices):
@@ -133,5 +131,5 @@ class SubscriptionItem(models.Model):
     subscription = models.ForeignKey(Subscription, on_delete=models.CASCADE)
     mirna = models.CharField(max_length=100, blank=False)
     gene = models.CharField(max_length=100, null=True)
-    record_date = models.DateTimeField(default=now())
+    record_date = models.DateTimeField(auto_now_add=True)
     unsubscribe_token = models.CharField(max_length=200, blank=False)
