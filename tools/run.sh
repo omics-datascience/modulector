14a09443cbd8391c04a39d4a835a7505ebd1d6d9
Julián Muhlberger <muhlber@gmail.com>
2022-03-15 17:01:39 -0300

    DB connection checks on start-up inside run.sh

--- tools/run.sh
+++ tools/run.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 logger "Starting app..."
 
+while ! python3 tools/test_db_connection.py
+do
+    echo "Database is still down, retrying in 5 seconds"
+    sleep 5
+done
+
+
 python3 manage.py generate_secret_key --settings='ModulectorBackend.settings' && python3 manage.py collectstatic --no-input && python3 manage.py makemigrations && python3 manage.py migrate && daphne -b 0.0.0.0 -p 8000 ModulectorBackend.asgi:application
