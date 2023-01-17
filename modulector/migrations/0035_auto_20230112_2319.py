f3376322c648e55a598e947f2cc81ec8fa418c6e
mauricio <mauribrunner@gmail.com>
2023-01-17 19:00:53 -0300

    Modelado de metilacion y carga de datos Illumina

--- modulector/migrations/0035_auto_20230112_2319.py
+++ modulector/migrations/0035_auto_20230112_2319.py
@@ -23,17 +23,21 @@ def import_methylation_epic_v2(apps, schema_editor):
     os.system('tail -n +8 ' + db_path + ' >modulector/files/tmp_db.csv')  # Elimino primeras 7 filas
 
     print("\nobteniendo modelo...")
-    methylationepic = apps.get_model(app_label='modulector', model_name='MethylationEPIC')
-    methylationisland = apps.get_model(app_label='modulector', model_name='MethylationUCSC_CPGIsland')
-    methylationrefgene = apps.get_model(app_label='modulector', model_name='MethylationUCSCRefGene')
-    methylationgencode = apps.get_model(app_label='modulector', model_name='MethylationGencode')
+    methylation_epic = apps.get_model(app_label='modulector', model_name='MethylationEPIC')
+    methylation_island = apps.get_model(app_label='modulector', model_name='MethylationUCSC_CPGIsland')
+    methylation_refgene = apps.get_model(app_label='modulector', model_name='MethylationUCSCRefGene')
+    methylation_gencode = apps.get_model(app_label='modulector', model_name='MethylationGencode')
+    methylation_epic.objects.all().delete()
+    methylation_island.objects.all().delete()
+    methylation_refgene.objects.all().delete()
+    methylation_gencode.objects.all().delete()
 
     print("proceso DB y cargo datos a tablas...")
     with open("modulector/files/tmp_db.csv", "r") as record:
         csvreader_object = csv.DictReader(record, dialect='excel', delimiter=",")
         for row in tqdm.tqdm(csvreader_object):
             if row["IlmnID"].startswith("cg"):
-                methyl_site = methylationepic.objects.create(
+                methyl_site = methylation_epic.objects.create(
                     ilmnid=row["IlmnID"],
                     name=row["Name"],
                     strand_fr=row["Strand_FR"],
@@ -46,49 +50,57 @@ def import_methylation_epic_v2(apps, schema_editor):
 
                 islands_values = row["UCSC_CpG_Islands_Name"].split(";")
                 relation_values = row["Relation_to_UCSC_CpG_Island"].split(";")
-                leng_islands_values = len(islands_values)
+                len_islands_values = len(islands_values)
                 len_relation_values = len(relation_values)
-                if leng_islands_values == len_relation_values and len_relation_values > 0:
-                    for i in range(0, len_relation_values):
-                        if islands_values[i].startswith("chr"):
-                            methylationisland.objects.create(
-                                ucsc_cpg_island_name=islands_values[i],
-                                relation_to_ucsc_cpg_island=relation_values[i],
-                                methylation_epic_v2_ilmnid=methyl_site
-                            )
+                if len_islands_values == len_relation_values and len_relation_values > 0:
+                    islands = [
+                        methylation_island(
+                            ucsc_cpg_island_name=islands_values[i],
+                            relation_to_ucsc_cpg_island=relation_values[i],
+                            methylation_epic_v2_ilmnid=methyl_site
+                        )
+                        for i in range(0, len_relation_values)
+                        if islands_values[i].startswith("chr")
+                    ]
+                    methylation_island.objects.bulk_create(islands)
 
                 ucscrefseq_group_values = row["UCSC_RefGene_Group"].split(";")
                 ucscrefseq_name_values = row["UCSC_RefGene_Name"].split(";")
-                ucscrefseq_accesion_values = row["UCSC_RefGene_Accession"].split(";")
+                ucscrefseq_accession_values = row["UCSC_RefGene_Accession"].split(";")
                 len_ucscrefseq_group_values = len(ucscrefseq_group_values)
                 len_ucscrefseq_name_values = len(ucscrefseq_name_values)
-                len_ucscrefseq_accesion_values = len(ucscrefseq_accesion_values)
-                if len_ucscrefseq_group_values == len_ucscrefseq_name_values and len_ucscrefseq_name_values == len_ucscrefseq_accesion_values:
-                    for i in range(0, len_ucscrefseq_name_values):
-                        if len(ucscrefseq_group_values[i]) > 0:  # chequea que tenga algo, ya que puede ser un str vacio
-                            methylationrefgene.objects.create(
-                                ucsc_refgene_group=ucscrefseq_group_values[i],
-                                ucsc_refgene_name=ucscrefseq_name_values[i],
-                                ucsc_refgene_accession=ucscrefseq_accesion_values[i],
-                                methylation_epic_v2_ilmnid=methyl_site
-                            )
+                len_ucscrefseq_accession_values = len(ucscrefseq_accession_values)
+                if len_ucscrefseq_group_values == len_ucscrefseq_name_values and len_ucscrefseq_name_values == len_ucscrefseq_accession_values:
+                    ucscrefseq = [
+                        methylation_refgene(
+                            ucsc_refgene_group=ucscrefseq_group_values[u],
+                            ucsc_refgene_name=ucscrefseq_name_values[u],
+                            ucsc_refgene_accession=ucscrefseq_accession_values[u],
+                            methylation_epic_v2_ilmnid=methyl_site
+                        )
+                        for u in range(0, len_ucscrefseq_name_values)
+                        if len(ucscrefseq_group_values[u]) > 0
+                    ]
+                    methylation_refgene.objects.bulk_create(ucscrefseq)
 
-                        gencode_group_values = row["GencodeV41_Group"].split(";")
-                        gencode_name_values = row["GencodeV41_Name"].split(";")
-                        gencode_accesion_values = row["GencodeV41_Accession"].split(";")
-                        len_gencode_group_values = len(gencode_group_values)
-                        len_gencode_name_values = len(gencode_name_values)
-                        len_gencode_accesion_values = len(gencode_accesion_values)
-                        if len_gencode_group_values == len_gencode_name_values and len_gencode_name_values == len_gencode_accesion_values:
-                            for i in range(0, len_gencode_name_values):
-                                if len(gencode_group_values[
-                                           i]) > 0:  # chequea que tenga algo, ya que puede ser un str vacio
-                                    methylationgencode.objects.create(
-                                        gencode_group=gencode_group_values[i],
-                                        gencode_name=gencode_name_values[i],
-                                        gencode_accession=gencode_accesion_values[i],
-                                        methylation_epic_v2_ilmnid=methyl_site
-                                    )
+                gencode_group_values = row["GencodeV41_Group"].split(";")
+                gencode_name_values = row["GencodeV41_Name"].split(";")
+                gencode_accession_values = row["GencodeV41_Accession"].split(";")
+                len_gencode_group_values = len(gencode_group_values)
+                len_gencode_name_values = len(gencode_name_values)
+                len_gencode_accession_values = len(gencode_accession_values)
+                if len_gencode_group_values == len_gencode_name_values and len_gencode_name_values == len_gencode_accession_values:
+                    gencode = [
+                        methylation_gencode(
+                            gencode_group=gencode_group_values[i],
+                            gencode_name=gencode_name_values[i],
+                            gencode_accession=gencode_accession_values[i],
+                            methylation_epic_v2_ilmnid=methyl_site
+                        )
+                        for i in range(0, len_gencode_name_values)
+                        if len(gencode_group_values[i]) > 0
+                    ]
+                    methylation_gencode.objects.bulk_create(gencode)
 
     os.remove("modulector/files/tmp_db.csv")
 
