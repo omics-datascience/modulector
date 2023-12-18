e266643c1e99391b90bd3737b6c1e550a56cf527
mauricio <mauribrunner@gmail.com>
2023-12-18 12:05:47 -0300

    methylation tests added

--- modulector/tests/test_methylation.py
+++ modulector/tests/test_methylation.py
@@ -1,4 +1,5 @@
 import json
+import sys
 from django.test import Client, TestCase
 
 client = Client()
@@ -80,166 +81,112 @@ class methylationTests(TestCase):
         self.assertTrue(len(data) == 1)
         self.assertIsInstance(list(data)[0], str)
 
+    """ Testing /methylation-sites-finder/ endpoint """
 
-""" Testing /methylation-sites-finder/ endpoint """
-""" Testing /methylation-sites/ endpoint """
-""" Testing /methylation-sites-genes/ endpoint """
+    def testMethylationSitesFinder1(self):
+        """ Tests with a valid response whit 7 results """
+        response = client.get(
+            '/methylation-sites-finder/', {'query': 'cg25', 'limit': 7})
+        self.assertEqual(response.status_code, 200)
+        data = response.data
+        self.assertIsInstance(data, list)
+        self.assertTrue(len(data) == 7)
 
-# def testMirnaList2(self):
-#     """Tests mirna endpoint with a valid mirna"""
-#     response = client.get('/mirna/', {'mirna': 'hsa-miR-548ai'})
-#     self.assertEqual(response.status_code, 200)
-#     # Checks all fields
-#     data = response.data
-#     self.assertTrue('aliases' in data)
-#     self.assertIsInstance(data['aliases'], list)
-#     self.assertTrue("MIMAT0018989" in data['aliases'])
+    def testMethylationSitesFinder2(self):
+        """ Tests default limit parameter value """
+        response = client.get('/methylation-sites-finder/', {'query': 'cg25'})
+        self.assertEqual(response.status_code, 200)
+        data = response.data
+        self.assertIsInstance(data, list)
+        self.assertTrue(len(data) == 50)
 
-#     self.assertTrue('mirna_sequence' in data)
-#     self.assertIsInstance(data['mirna_sequence'], str)
-#     self.assertEqual(data['mirna_sequence'], "AAAGGUAAUUGCAGUUUUUCCC")
+    def testMethylationSitesFinder3(self):
+        """ Tests endpoint without parameters """
+        response = client.get('/methylation-sites-finder/')
+        self.assertEqual(response.status_code, 200)
+        data = response.data
+        self.assertIsInstance(data, list)
+        self.assertTrue(len(data) == 0)
 
-#     self.assertTrue('mirbase_accession_id' in data)
-#     self.assertIsInstance(data['mirbase_accession_id'], str)
-#     self.assertEqual(data['mirbase_accession_id'], "MIMAT0018989")
+    """ Testing /methylation-sites/ endpoint """
 
-#     self.assertTrue('links' in data)
-#     self.assertIsInstance(data['links'], list)
-#     self.assertTrue(len(data['links']) == 1)
-#     self.assertIsInstance(data['links'][0], dict)
-#     self.assertTrue('source' in data['links'][0])
-#     self.assertTrue('url' in data['links'][0])
-#     self.assertIsInstance(data['links'][0]['source'], str)
-#     self.assertIsInstance(data['links'][0]['url'], str)
+    def testMethylationSites1(self):
+        """ Tests with a valid body """
+        data_body = json.dumps(
+            {
+                "methylation_sites": [
+                    "cg22461615_TC11",
+                    "cg01615704_TC11",
+                    "cg25908985",
+                    "invalid_data"]
+            }
+        )
+        response = client.post('/methylation-sites/', data=data_body,
+                               content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        data = response.data
+        # sys.exit(str(data))
+        for k in data:
+            self.assertIsInstance(data[k], list)
+        self.assertTrue("cg22461615_TC11" in data)
+        self.assertTrue("cg01615704_TC11" in data)
+        self.assertTrue("cg25908985" in data)
+        self.assertTrue("invalid_data" in data)
+        # self.assertEqual(data["cg22461615_TC11"], ["cg22461615"])
+        # self.assertEqual(data["cg01615704_TC11"], ["cg01615704"])
+        # self.assertEqual(data["cg25908985"], ["cg25908985"])
+        # self.assertEqual(data["invalid_data"], [])
 
-# def testMirnaList3(self):
-#     """Tests 404 error for mirna endpoint due to not specify the 'mirna' parameter"""
-#     response = client.get('/mirna/')
-#     self.assertEqual(response.status_code, 404)
+    def testMethylationSites2(self):
+        """ Tests with a invalid body type """
+        data = json.dumps({"methylation_sites": "cg01615704_TC11"})
+        response = client.post('/methylation-sites/', data=data,
+                               content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+        data = response.data
+        self.assertTrue("detail" in data)
 
-# """ Testing /mirna-target-interactions/ endpoint """
+    def testMethylationSites3(self):
+        """ Tests with a invalid body key """
+        data = json.dumps({"methylation": ["cg01615704_TC11"]})
+        response = client.post('/methylation-sites/', data=data,
+                               content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+        data = response.data
+        self.assertTrue("detail" in data)
 
-# def testMirnaTargetInteractions1(self):
-#     """Tests with a invalid mirna"""
-#     response = client.get(
-#         '/mirna-target-interactions/', {'mirna': 'goku_capo'})
-#     self.assertEqual(response.status_code, 200)
-#     self.__check_pagination(response)
-#     self.__check_empty_pagination(response)
+    """ Testing /methylation-sites-genes/ endpoint """
 
-# def testMirnaTargetInteractions2(self):
-#     """Tests with a valid mirna and gene"""
-#     response = client.get(
-#         '/mirna-target-interactions/', {'mirna': 'hsa-miR-891a-5p', 'gene': 'EGFR'})
-#     self.assertEqual(response.status_code, 200)
-#     self.__check_pagination(response)
-#     self.__check_one_result_pagination(response)
-#     # Checks all fields
-#     data = response.data['results'][0]
-#     self.assertIsInstance(data, dict)
+    def testMethylationSitesToGenes1(self):
+        """ Tests with a valid body """
+        data_body = json.dumps(
+            {"methylation_sites": ["cg17771854_BC11", "cg22461615", "name_007"]})
+        response = client.post('/methylation-sites-genes/', data=data_body,
+                               content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        data = response.data
+        # sys.exit(str(data))
+        self.assertTrue("cg17771854_BC11" in data)
+        self.assertTrue("cg22461615" in data)
+        for k in data:
+            self.assertIsInstance(data[k], list)
+        self.assertEqual(data["cg17771854_BC11"], ["IPO13"])
+        self.assertEqual(data["cg22461615"], ["THAP9", "THAP9-AS1", "SEC31A"])
 
-#     self.assertTrue('id' in data)
-#     self.assertIsInstance(data['id'], int)
+    def testMethylationSitesToGenes2(self):
+        """ Tests with a invalid body type """
+        data = json.dumps({"methylation_sites": "cg17771854_BC11"})
+        response = client.post('/methylation-sites-genes/', data=data,
+                               content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+        data = response.data
+        self.assertTrue("detail" in data)
 
-#     self.assertTrue('mirna' in data)
-#     self.assertIsInstance(data['mirna'], str)
-
-#     self.assertTrue('gene' in data)
-#     self.assertIsInstance(data['gene'], str)
-
-#     self.assertTrue('score' in data)
-#     self.assertIsInstance(data['score'], str)
-
-#     self.assertTrue('source_name' in data)
-#     self.assertIsInstance(data['source_name'], str)
-
-#     self.assertTrue('pubmeds' in data)
-#     self.assertIsInstance(data['pubmeds'], set)
-
-#     self.assertTrue('sources' in data)
-#     self.assertIsInstance(data['sources'], list)
-
-#     self.assertTrue('score_class' in data)
-#     self.assertIsInstance(data['score_class'], str)
-
-# def testMirnaTargetInteractions3(self):
-#     """Tests 400 error for mirna endpoint due to not specify parameters"""
-#     response = client.get('/mirna-target-interactions/')
-#     self.assertEqual(response.status_code, 400)
-
-# """ Testing /mirna-aliases/ endpoint """
-
-# def testMirnaAliases1(self):
-#     """ Test the entire mirna-aliases/ endpoint since it does not receive parameters """
-#     response = client.get('/mirna-aliases/')
-#     self.assertEqual(response.status_code, 200)
-#     self.__check_pagination(response)
-
-# """ Testing /mirna-codes/ endpoint """
-
-# def testMirnaCodes1(self):
-#     """ Tests with a valid body """
-#     data = json.dumps({"mirna_codes": ["name_01", "Hsa-Mir-935-v2_5p*",
-#                                        "MIMAT0000066",
-#                                        "MI0026417",
-#                                        "hsa-let-7e-5p"]
-#                        })
-#     response = client.post('/mirna-codes/', data=data,
-#                            content_type='application/json')
-#     self.assertEqual(response.status_code, 200)
-#     data = response.data
-#     self.assertTrue("name_01" in data)
-#     self.assertTrue("Hsa-Mir-935-v2_5p*" in data)
-#     self.assertTrue("MIMAT0000066" in data)
-#     self.assertTrue("MI0026417" in data)
-#     self.assertTrue("hsa-let-7e-5p" in data)
-#     self.assertIsNone(data["name_01"])
-#     self.assertIsNone(data["Hsa-Mir-935-v2_5p*"])
-#     self.assertEqual(data["MIMAT0000066"], "MIMAT0000066")
-#     self.assertEqual(data["MI0026417"], "MI0026417")
-#     self.assertEqual(data["hsa-let-7e-5p"], "MIMAT0000066")
-
-# def testMirnaCodes2(self):
-#     """ Tests with a invalid body type """
-#     data = json.dumps({"mirna_codes": "Hsa-Mir-935-v2_5p*"})
-#     response = client.post('/mirna-codes/', data=data,
-#                            content_type='application/json')
-#     self.assertEqual(response.status_code, 400)
-#     data = response.data
-#     self.assertTrue("detail" in data)
-
-# def testMirnaCodes3(self):
-#     """ Tests with a invalid body key """
-#     data = json.dumps({"mirna": ["Hsa-Mir-935-v2_5p*"]})
-#     response = client.post('/mirna-codes/', data=data,
-#                            content_type='application/json')
-#     self.assertEqual(response.status_code, 400)
-#     data = response.data
-#     self.assertTrue("detail" in data)
-
-# """ Testing /mirna-codes-finder/ endpoint """
-
-# def testMirnaCodesFinder1(self):
-#     """ Tests with a valid response whit 7 results """
-#     response = client.get(
-#         '/mirna-codes-finder/', {'query': 'hsa-', 'limit': 7})
-#     self.assertEqual(response.status_code, 200)
-#     data = response.data
-#     self.assertIsInstance(data, list)
-#     self.assertTrue(len(data) == 7)
-
-# def testMirnaCodesFinder2(self):
-#     """ Tests default limit parameter value """
-#     response = client.get('/mirna-codes-finder/', {'query': 'hsa-'})
-#     self.assertEqual(response.status_code, 200)
-#     data = response.data
-#     self.assertIsInstance(data, list)
-#     self.assertTrue(len(data) == 50)
-
-# def testMirnaCodesFinder3(self):
-#     """ Tests endpoint without parameters """
-#     response = client.get('/mirna-codes-finder/')
-#     self.assertEqual(response.status_code, 200)
-#     data = response.data
-#     self.assertIsInstance(data, list)
-#     self.assertTrue(len(data) == 0)
+    def testMethylationSitesToGenes3(self):
+        """ Tests with a invalid body key """
+        data = json.dumps({"methylation": "cg17771854_BC11"})
+        response = client.post('/methylation-sites-genes/', data=data,
+                               content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+        data = response.data
+        self.assertTrue("detail" in data)
